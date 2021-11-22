e19056ab4c3b286451d4ccb5aa85b31f722a6c01
sammygelman <sammygelman@gmail.com>
2021-11-22 14:45:39 +0200

    80 epoch images added

--- src/test.py
+++ src/test.py
@@ -1,15 +1,15 @@
 import os
 import pandas as pd
 
-num_poems = 17
-df = pd.read_csv("shuffled_poems.csv")
+num_poems = 712
+df = pd.read_csv("/home/sami_bb/simply/src/updated_twitter.csv")
 
 for i in range(num_poems):
-    poem = df["poems"][700+i]
-    action = os.system('mkdir %s' % (str(700+i)))
-    action2 = os.system('python /home/sami_bb/VQGAN-CLIP/generate.py -p "%s" -s 575 575 -se 25 -i 575' % (str(poem)))
-    action3 = os.system('mv output.png %s' % (str(700+i)))
+    poem = df["poems"][i]
+    action = os.system('mkdir /home/sami_bb/simply/images_2/%s' % (str(i)))
+    action2 = os.system('python /home/sami_bb/VQGAN-CLIP/generate.py -p "%s" -s 575 575 -i 80 -se 10' % (str(poem)))
+    action3 = os.system('mv output.png /home/sami_bb/simply/images_2/%s' % (str(i)))
     with open('poem.txt', 'w') as f:
             f.write(poem)
-    action4 = os.system('mv poem.txt %s' % (str(700+i)))
+    action4 = os.system('mv poem.txt /home/sami_bb/simply/images_2/%s' % (str(i)))
 
